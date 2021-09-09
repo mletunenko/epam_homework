8f232b574216ddc96376cf1d58be1a375eb6e332
maria <@.com>
2021-09-09 10:34:04 +0300

    prepared second homewokr

--- homework2/task01.py
+++ homework2/task01.py
@@ -10,9 +10,33 @@ from typing import List
 
 
 def get_longest_diverse_words(file_path: str) -> List[str]:
+    storage = []
     with open(file_path, encoding='unicode-escape') as file:
-        pass
-
+        for line in file:
+            for word in line.split():
+                # Cut the punctuation from word
+                word = word.strip("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~""")
+                number = len(set(word))
+                # Check if storage less then 10 words
+                if len(storage) < 10:
+                    # Put the word into storage
+                    storage.append((number, word))
+                    # Check if storage have 10 words
+                    if len(storage) == 10:
+                        # Sort a storage, to have shortest word in zero pos
+                        storage.sort(key=lambda x: x[0], reverse=True)
+                # Check if word enough good to put in storage
+                elif number > storage[-1][0]:
+                    # Looking for place for new word
+                    for index, (stored_number, stored_word) in enumerate(
+                            storage):
+                        # Insert word by len
+                        if number > stored_number:
+                            storage.insert(index, (number, word))
+                            # Take the 10 longest word in storage
+                            storage = storage[:10]
+                            break
+    return [elem[1] for elem in storage]
 
 
 def get_rarest_char(file_path: str) -> str:
@@ -20,11 +44,17 @@ def get_rarest_char(file_path: str) -> str:
     count_dict = {}
     # Open file data.txt
     with open(file_path, encoding='unicode-escape') as file:
+        # Try to take a first char as rarest
         rarest_char = file.read(1)
+        # Check if file is not empty
+        if not rarest_char:
+            return None
+        # Move seek to the beginning of file
+        file.seek(0)
         while True:
             # Read a file line by line
             line = file.readline()
-            # Stop While loop if a file is ended
+            # Stop reading if a file is ended
             if not line:
                 break
             # Read line char by char
@@ -42,12 +72,56 @@ def get_rarest_char(file_path: str) -> str:
 
 
 def count_punctuation_chars(file_path: str) -> int:
-    pass
+    # Know every punctuation char
+    punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+    # Declare a counter
+    counter = 0
+    # Open a file
+    with open(file_path, encoding='unicode-escape') as file:
+        while True:
+            # Read a file line by line
+            line = file.readline()
+            # Stop reading if a file is ended
+            if not line:
+                break
+            # Read a line char by char
+            for char in line:
+                # Check if char is punctuation
+                if char in punctuation:
+                    counter += 1
+    return counter
 
 
 def count_non_ascii_chars(file_path: str) -> int:
-    pass
+    # Create a counter
+    counter = 0
+    with open(file_path, encoding='unicode-escape') as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            for char in line:
+                # Check if char is not ascii char
+                if not char.isascii():
+                    # Count if not
+                    counter += 1
+    return counter
 
 
 def get_most_common_non_ascii_char(file_path: str) -> str:
-    pass
+    counter_dict = {}
+    with open(file_path, encoding='unicode-escape') as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            for char in line:
+                if not char.isascii():
+                    if char not in counter_dict:
+                        counter_dict[char] = 0
+                    counter_dict[char] += 1
+    if counter_dict:
+        most_common_char = max(counter_dict.items(), key=lambda x: x[1])[0]
+    else:
+        most_common_char = None
+    return most_common_char
--- tests/homework2/test_task01.py
+++ tests/homework2/test_task01.py
@@ -1,5 +1,103 @@
-from homework2.task01 import get_rarest_char
+import tempfile
+
+from homework2.task01 import (count_non_ascii_chars, count_punctuation_chars,
+                              get_longest_diverse_words,
+                              get_most_common_non_ascii_char, get_rarest_char)
+
+
+def test_get_longest_diverse_word_data_txt_test():
+    assert get_longest_diverse_words('homework2/data.txt') == \
+           ['unmißverständliche', 'Bevölkerungsabschub', 'Kollektivschuldiger',
+            'Werkstättenlandschaft', 'Schicksalsfiguren',
+            'politisch-strategischen',
+            'Selbstverständlich', 'Werkstättenlandschaft',
+            'résistance-Bewegungen',
+            'Fingerabdrucks']
+
+
+def test_get_longest_diverse_word_10_word():
+    with tempfile.NamedTemporaryFile(mode='w') as temporary_file:
+        temporary_file.write("""Lorem ipsum dolor sit amet, consectetur 
+        adipiscing elit. Nunc blandit.""")
+        temporary_file.flush()
+        assert set(get_longest_diverse_words(temporary_file.name)) == \
+               {'Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur',
+                'adipiscing', 'elit', 'Nunc', 'blandit'}
+
+
+def test_get_longest_diverse_many_punctuation():
+    with tempfile.NamedTemporaryFile(mode='w') as temporary_file:
+        temporary_file.write("""
+        @Lorem@ ipsum$% &* )(dolor": sit<>< <amet, 
+        ><consectetur adipiscing>< elit. Nunc^&* %^ blandit.
+        """)
+        temporary_file.flush()
+        assert set(get_longest_diverse_words(temporary_file.name)) == \
+               {'Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur',
+                'adipiscing', 'elit', 'Nunc', 'blandit'}
 
 
 def test_get_rarest_char_common_test():
     assert get_rarest_char('homework2/data.txt') == '›'
+
+
+def test_get_rarest_char_in_single_char_file():
+    with tempfile.NamedTemporaryFile(mode='w') as temporary_file:
+        temporary_file.write('a')
+        temporary_file.flush()
+        assert get_rarest_char(temporary_file.name) == 'a'
+
+
+def test_get_rarest_char_in_empty_file():
+    with tempfile.NamedTemporaryFile() as temporary_file:
+        assert get_rarest_char(temporary_file.name) == None
+
+
+def test_count_punctuation_common_test():
+    assert count_punctuation_chars('homework2/data.txt') == 5305
+
+
+def test_count_punctuation_in_single_char_file():
+    with tempfile.NamedTemporaryFile(mode='w') as temporary_file:
+        temporary_file.write("*")
+        temporary_file.flush()
+        assert count_punctuation_chars(temporary_file.name) == 1
+
+
+def test_count_punctuation_in_empty_file():
+    with tempfile.NamedTemporaryFile() as temporary_file:
+        assert count_punctuation_chars(temporary_file.name) == 0
+
+
+def test_count_non_ascii_common_test():
+    assert count_non_ascii_chars('homework2/data.txt') == 2972
+
+
+def test_count_non_ascii_in_only_ascii_char_file():
+    with tempfile.NamedTemporaryFile(mode='w') as temporary_file:
+        temporary_file.write('Lorem ipsum dolor sit amet, consectetuer '
+                             'adipiscing elit. Aenean commodo ligula '
+                             'eget dolor.')
+        temporary_file.flush()
+        assert count_non_ascii_chars(temporary_file.name) == 0
+
+
+def test_count_non_ascii_in_empty_file():
+    with tempfile.NamedTemporaryFile() as temporary_file:
+        assert count_non_ascii_chars(temporary_file.name) == 0
+
+
+def test_get_most_common_non_ascii_char_common_test():
+    assert get_most_common_non_ascii_char('homework2/data.txt') == 'ä'
+
+
+def test_get_most_common_non_ascii_char_empty_file():
+    with tempfile.NamedTemporaryFile() as empty_file:
+        assert get_most_common_non_ascii_char(empty_file.name) == None
+
+
+def test__get_most_common_non_ascii_char_only_ascii():
+    with tempfile.NamedTemporaryFile(mode='w') as temporary_file:
+        temporary_file.write('Qwerty 1234 <>?:"')
+        temporary_file.flush()
+        assert get_most_common_non_ascii_char(temporary_file.name) == None
